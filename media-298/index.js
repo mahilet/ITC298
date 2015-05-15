b97319985229a41db7a2641520feef1c5be45b4e
mahilet <mehalu15jan2013@gmail.com>
2015-05-14 17:03:27 -0700

    Revert "I add Gruntfile"
    This reverts commit 375fea93989190c13683ec0f4d29b5594cf0bb2a.

--- media-298/index.js
+++ media-298/index.js
@@ -1,64 +1,77 @@
-var hapi = require("hapi");
 var fs = require("fs");
+var hapi = require("hapi");
 var server = new hapi.Server();
-server.connection({port: 8000});
+server.connection({ port: 8000});
 server.start();
 
 server.views({
-    path: "templates",
-    engines: {
-        html:require("handlebars")
-    },
-    layoutPath: "layouts",
-    layout: "default"
+  path: "templates",
+  engines: {
+    html:require("handlebars")
+  },
+  isCached: false,
+  layoutPath: "layouts",//llets it know that 'layouts folder exists'
+  layout: "default"
+
 });
-//
-// server.route({
-//     method: "GET",
-//     path: "/",
-//     handler: function(req, reply) {
-//        reply.view("index", {
-//         title: "Home"
-//        });
-//     }
-//
-// });
-
-
-var JsonMovies;
-fs.readFile("media.json", "utf8", function(err, data) {
-    JsonMovies = JSON.parse(data).movies;
-});
-
-//fs.readFile(file, [encoding], [callback]);
-server.route({
-    method: "GET",
-    path: "/",
-    handler: function(req, reply) {
-        reply.view("movies", {//this is movies.html
-            title: "Movies",
-            movies: JsonMovies
-        });
-    }
-});
-
 
 server.route({
-    method: "GET",
-    path: "/movie/{index}",
-     handler: function(req, reply) {
-        var movie = JsonMovies[req.params.index];
+  method: "GET",
+  path: "/",
+  handler: function(req, reply) {
+    reply.view("index.html", {
+      title: "home"
+    });
+  }
+});
+// this is the route for my movies page
+server.route({
+  method:"GET",
+  path: "/movies",
+  handler: function(req, reply){
+    fs.readFile("media2.json", "utf8", function(err, data) {
 
-
-        reply.view("movie", {//this is movie.html
-            title: movie.title,
-            times: movie.times
-        });
-    }
+      reply.view("movies.html", {
+        title: "movies",
+        //BELOW IS PROPERTY THAT COMMUNTICATES WITH THE 'SECTION' IN HANDLEBARS
+        movies: JSON.parse(data).media.movies
+      });
+    });
+  }
 });
 
+// this is the route for my books page
+server.route({
+  method: "GET",
+  path: "/books",
+  handler: function(req, reply){
+    fs.readFile("media2.json", "utf8", function(err, data) {
+
+      reply.view("books.html", {
+        title: "books",
+        //BELOW IS PROPERTY THAT COMMUNTICATES WITH THE 'SECTION' IN HANDLEBARS
+        books: JSON.parse(data)media.books
+      });
+    });
+  }
+  });
 
 
+// this is the route for my tv shows
+server.route({
+  method: "GET",
+  path: "/tvshows",
+  handler: function(req, reply){
+    fs.readFile("media2.json", "utf8", function(err, data) {
+
+      reply.view("tvshows.html", {
+        title: "tvshows",
+        //BELOW IS PROPERTY THAT COMMUNTICATES WITH THE 'SECTION' IN HANDLEBARS
+        tvshows: JSON.parse(data).media.tvshows
+      });
+    });
+  }
+  });
 
 
 server.route({
