e8f12f7eec3934eb9d41afbc5448f87816201a35
mahilet <mehalu15jan2013@gmail.com>
2015-05-14 17:20:13 -0700

    I add Gruntfile
    I add Gruntfile

---
+++ old/media-298/Gruntfile.js
@@ -0,0 +1,44 @@
+//Gruntfile
+
+module.exports = function(grunt) {
+
+
+  grunt.loadNpmTasks("grunt-autoprefixer");
+
+
+// grunt.registerTask("default",["autoprefixer",]);
+grunt.registerTask("default","autoprefixer");
+
+// initialize means start
+grunt.initConfig({
+  //this is where i will add all my grunt- nodes
+
+
+  nodemon:{
+        dev:{
+             script:"index.js"
+        }
+    },
+
+
+
+
+
+
+//this will automate making my CSS work for MS IE, Mozilla firefox and google
+  autoprefixer: {
+     dev: {
+       expand: true,
+       flatten: true,
+       //source folder this is your CSS
+       src:"public/**/*.css",
+       //destination folder this the automated
+       dest:"build/css/"
+     }
+   }
+
+
+});
+
+
+};
---
+++ old/media-298/index.js
@@ -0,0 +1,103 @@
+var fs = require("fs");
+var hapi = require("hapi");
+var server = new hapi.Server();
+server.connection({ port: 8000});
+server.start();
+
+server.views({
+  path: "templates",
+  engines: {
+    html:require("handlebars")
+  },
+  isCached: false,
+  layoutPath: "layouts",//llets it know that 'layouts folder exists'
+  layout: "default"
+
+});
+
+server.route({
+  method: "GET",
+  path: "/",
+  handler: function(req, reply) {
+    reply.view("index.html", {
+      title: "home"
+    });
+  }
+});
+// this is the route for my movies page
+server.route({
+  method:"GET",
+  path: "/movies",
+  handler: function(req, reply){
+    fs.readFile("media.json", "utf8", function(err, data) {
+
+      reply.view("movies.html", {
+        title: "movies",
+        //BELOW IS PROPERTY THAT COMMUNTICATES WITH THE 'SECTION' IN HANDLEBARS
+        movies: JSON.parse(data).movies
+      });
+    });
+  }
+});
+
+// this is the route for my books page
+server.route({
+  method: "GET",
+  path: "/books",
+  handler: function(req, reply){
+    fs.readFile("media.json", "utf8", function(err, data) {
+
+      reply.view("books.html", {
+        title: "books",
+//BELOW IS PROPERTY THAT COMMUNTICATES WITH THE 'SECTION' IN HANDLEBARS
+        books: JSON.parse(data).books
+      });
+    });
+  }
+  });
+
+
+
+
+// this is the route for my tv shows
+server.route({
+  method: "GET",
+  path: "/tvshows",
+  handler: function(req, reply){
+    fs.readFile("media.json", "utf8", function(err, data) {
+
+      reply.view("tvshows.html", {
+        title: "tvshows",
+        //BELOW IS PROPERTY THAT COMMUNTICATES WITH THE 'SECTION' IN HANDLEBARS
+        ralph: JSON.parse(data).tvshows
+      });
+    });
+  }
+  });
+
+  server.route({
+    method: "GET",
+    path:"/movies/interstellar",
+    handler: function(req, reply){
+      fs.readFile("media.json", "utf8", function(err, data){
+        reply.view("interstellar.html",{
+          title: "interstellar",
+          singleMovie:JSON.parse(data).movies[0]
+
+
+        });
+      });
+    }
+    });
+
+
+server.route({
+  method:"GET",
+  path:"/assets/{param*}",
+  //configuration object here instead of a function
+  handler:{
+    directory: {
+      path: "public"
+    }
+  }
+});
