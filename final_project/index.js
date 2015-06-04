7ff9cd5d12b40fb92463219ea7001024ac3dd7f2
cluetrain <greenstrat67@gmail.com>
2015-06-04 15:56:11 -0700

    Added invalid login message

--- final_project/index.js
+++ final_project/index.js
@@ -1,8 +1,5 @@
 //index.js
 
-
-
-
 var fs = require("fs");
 var hapi = require("hapi");
 var server = new hapi.Server();
@@ -15,37 +12,33 @@ var sqlite = require("sqlite3");
 server.connection({port:8000});
 server.start();
 
-
 //------------ADDING SQLITE3------------------------
 
 var db = new sqlite.Database("database.db", function(){
 
-    db.run("CREATE TABLE IF NOT EXISTS users (name, comment)", function(){
+  db.run("CREATE TABLE IF NOT EXISTS users (name, comment)", function(){
 
-        db.run("INSERT INTO users VALUES ('Mahilet' , 'blah,blah,blah')");
-        
-        db.get("SELECT * FROM users", function(err,result){
-            console.log(result);
-        });
+    db.run("INSERT INTO users VALUES ('Mahilet' , 'blah,blah,blah')");
 
-        db.all("SELECT * FROM users", function(err,results){
-          console.log(results);
-        });
+    db.get("SELECT * FROM users", function(err,result){
+      console.log(result);
     });
-    db.run("CREATE TABLE IF NOT EXISTS auth (username, password)", function(){
 
-      db.run("INSERT INTO auth VALUES ('h', '1')");
-
-      db.get("SELECT * FROM auth", function(err,result){
-        console.log(result);
-      });
+    db.all("SELECT * FROM users", function(err,results){
+      console.log(results);
     });
+  });
+  db.run("CREATE TABLE IF NOT EXISTS auth (username, password)", function(){
+
+    db.run("INSERT INTO auth VALUES ('h', '1')");
+
+    db.get("SELECT * FROM auth", function(err,result){
+      console.log(result);
+    });
+  });
 });
 
 //------------END ADDING SQLITE3------------------------
-
-
-
 
 var allJson;
 fs.readFile("tempPosts.json", "utf8", function(err, data){
@@ -65,18 +58,12 @@ server.views({
 });
 
 server.route({
-      method:"GET",
-      path: "/",
-      handler: function(req, reply){
-        reply.view("login");
-      }
-    });
-
-
-
-
-
-
+  method:"GET",
+  path: "/",
+  handler: function(req, reply){
+    reply.view("login");
+  }
+});
 
 
 //-------------------TEMP LOGIN SERVER ROUTES--------
@@ -84,9 +71,10 @@ server.route({
   method:"GET",
   path: "/login",
   handler: function(req, reply){
-      reply.view("login");
-}
+    reply.view("login");
+  }
 });
+
 //THIS BELOW IS TEMPORARY it should be a db
 var fusers = {
   h: "1",
@@ -100,28 +88,24 @@ server.route({
   path:"/login",
   handler:function(req, reply){
     console.log(req.payload.user);
-      
-    var user = db.get("SELECT * FROM auth WHERE username = $username", {
-        $username : req.payload.user
-    }, function(err, result) {
-        console.log(this, result, req.payload.password);
-        if(req.payload.password == result.password) {
 
-          var response = reply.view("bloglist.html");
-        } else {
-            reply.redirect("/login");
-        }
+    var user = db.get("SELECT * FROM auth WHERE username = $username", {
+      $username : req.payload.user
+    }, function(err, result) {
+      console.log(this, result, req.payload.password);
+      if(req.payload.password == result.password) {
+        var response = reply.view("bloglist.html");
+      } else {
+        reply.view("/login", {
+          error: "Invalid Login",
+          username: req.payload.user,
+          password: req.payload.password
+        });
+      }
     });
 
-    }
+  }
 });
-
-
-
-
-
-
-
 
 //-------------------END TEMP LOGIN SERVER ROUTES--------
 
@@ -141,25 +125,25 @@ server.route ({
 // -----HIS IS WHERE I STARRT TO SAVE DATA TO JSON-----------------
 
 server.route ({
-      method: "POST",
-      path:"/createpost",
-      handler:function(req,reply) {
+  method: "POST",
+  path:"/createpost",
+  handler:function(req,reply) {
 
-        var namePost = req.payload;
-        console.log(namePost);
-        reply.view("bloglist.html",{
-           list: allJson
+    var namePost = req.payload;
+    console.log(namePost);
+    reply.view("bloglist.html",{
+      list: allJson
 
 
-          // blogs: posting.tempPostLists
-        })
+      // blogs: posting.tempPostLists
+    })
 
-        allJson.push(namePost);
+    allJson.push(namePost);
 
-        fs.writeFile('tempPosts.json', JSON.stringify(allJson), function (err) {
-          if (err) throw err;
-          console.log('The ' + namePost );
-      });
+    fs.writeFile('tempPosts.json', JSON.stringify(allJson), function (err) {
+      if (err) throw err;
+      console.log('The ' + namePost );
+    });
   }
 });
 
@@ -186,17 +170,17 @@ server.route({
   method:"GET",
   path: "/bios/bio/{index}",
   handler: function(req, reply){
-      var thisObject = allJson[req.params.index];
-      reply.view("bio.html",{
-        icon: thisObject.icon,
-        website: thisObject.mywebsite,
-        pic: thisObject.pic,
-        bio: thisObject.bio,
-        name: thisObject.username
+    var thisObject = allJson[req.params.index];
+    reply.view("bio.html",{
+      icon: thisObject.icon,
+      website: thisObject.mywebsite,
+      pic: thisObject.pic,
+      bio: thisObject.bio,
+      name: thisObject.username
 
-     });
+    });
 
-    }
+  }
 });
 
 
@@ -204,12 +188,12 @@ server.route({
 
 server.route ({
   method:"GET",
-   path:"/assets/{param*}",
-   handler:{
-       directory:{
-       path:"src"
+  path:"/assets/{param*}",
+  handler:{
+    directory:{
+      path:"src"
 
 
-       }
-   }
+    }
+  }
 });
